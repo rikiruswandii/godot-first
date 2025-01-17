ff0c6d4dd64e431536bceafb9f6e6f51f4dfc4b0
rikiruswandii <rikiruswandi28@gmail.com>
2025-01-17 14:51:26 +0700

    feat:more action

--- godot-first-script/godot-first.gd
+++ godot-first-script/godot-first.gd
@@ -1,39 +1,42 @@
-extends Node2D
+extends Node2D  # Tetap menggunakan Node2D
 
-@export var speed = 400
+@export var speed = 100
 var screen_size
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
-	
-func _process(delta: float) -> void:
 
+func _physics_process(delta: float) -> void:
 	var velocity = Vector2.ZERO
 	
+	# Mengatur arah gerakan
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
-		$Samurai.flip_h = false
+		$SamuraiBody/Samurai.flip_h = false
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
-		$Samurai.flip_h = true
+		$SamuraiBody/Samurai.flip_h = true
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
 
+	# Pergerakan dan Animasi pada CharacterBody2D
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-		position += velocity * delta
-		position = position.clamp(Vector2.ZERO, screen_size)
+		$SamuraiBody.velocity = velocity  # Mengatur velocity untuk CharacterBody2D
+		$SamuraiBody.move_and_slide()     # Panggil move_and_slide dari CharacterBody2D
 		if velocity.y != 0:
-			$Samurai.play("walk")
+			$SamuraiBody/Samurai.play("walk")
 		else:
-			$Samurai.play("run")
+			$SamuraiBody/Samurai.play("run")
 	else:
-		$Samurai.play("idle")
+		$SamuraiBody/Samurai.play("idle")
 
 	# Animasi serangan
 	if Input.is_action_just_pressed("left_click"):
-		$Samurai.play("attack_1")
+		$SamuraiBody/Samurai.play("attack_1")
 	elif Input.is_action_just_pressed("right_click"):
-		$Samurai.play("attack_2")
+		$SamuraiBody/Samurai.play("attack_2")
+	elif Input.is_action_just_pressed("ui_select"):
+		$SamuraiBody/Samurai.play("jump")
